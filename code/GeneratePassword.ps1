16664d4217fdfa0d42d6fdf9680a6ab4d8c23a18
Marvin Buss <marvin.buss@gmail.com>
2020-09-22 20:36:43 +0200

    updated pws code

--- code/GeneratePassword.ps1
+++ code/GeneratePassword.ps1
@@ -1,3 +1,18 @@
+# Define script arguments
+param (
+    [Parameter(Mandatory = $true)]
+    [String]
+    $VaultName,
+
+    [Parameter(Mandatory = $true)]
+    [String]
+    $SecretName,
+
+    [Parameter(Mandatory=$false)]
+    [Switch]
+    $Force
+)
+
 function New-Password {
     <#
         .SYNOPSIS
@@ -176,4 +191,21 @@ function New-Password {
     }
 }
 
-Export-ModuleMember -Function New-Password
+# Get secret from key vault
+Write-Output "Getting secret from key vault"
+$Secret = Get-AzKeyVaultSecret -VaultName $VaultName -Name $SecretName
+
+if (($Null -eq $Secret) -or ($Force)) {
+    Write-Output "Secret does not exist yet. Creating new secret with name $SecretName."
+
+    # Generate password
+    Write-Output "Generating password"
+    $Password = New-Password
+
+    # Create secret in key vault
+    Write-Output "Creating secret in key vault"
+    Set-AzKeyVaultSecret -VaultName $VaultName -Name $SecretName -SecretValue $Password
+}
+else {
+    Write-Output "Secret already exists. No need to create a new secret with name $SecretName."
+}
