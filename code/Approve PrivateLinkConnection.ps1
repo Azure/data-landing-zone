06b8d96706bdf3da08db392837c432229d5369e0
Marvin Buss <marvin.buss@gmail.com>
2020-10-23 08:17:22 +0200

    add script for private link endpoint approval

---
+++ code/Approve PrivateLinkConnection.ps1
@@ -0,0 +1,18 @@
+# Define script arguments
+param (
+    [Parameter(Mandatory = $true)]
+    [String]
+    $ResourceId
+)
+
+# Get Private Link Endpoint Connection
+Write-Host "Getting Private Link Endpoint Connection"
+$privateEndpoints = Get-AzPrivateEndpointConnection -PrivateLinkResourceId $ResourceId
+$privateEndpoints
+
+# Approve all Private Link Endpoint Connection
+Write-Host "Approving all Private Link Endpoint Connections"
+foreach ($privateEndpoint in $privateEndpoints) {
+	$id = $privateEndpoint.id
+	Approve-AzPrivateEndpointConnection -ResourceId "${id}"
+}
